3ad0c002efc47ebd7162111acb3d259a994eadec
JooYoung2274 <jooooo2274@gmail.com>
2023-12-26 15:14:25 +0900

    nestjs-query 02_Hook BeforeCreateOneHook, BeforeUpdateOneHook 적용

--- src/domain/board/dto/create-board.dto.ts
+++ src/domain/board/dto/create-board.dto.ts
@@ -1,7 +1,10 @@
 import { Field, InputType } from '@nestjs/graphql';
+import { BeforeCreateOne } from '@ptc-org/nestjs-query-graphql';
 import { IsBoolean, IsString } from 'class-validator';
+import { CreatedByHook } from './nestjs-query-before-create-one.hook';
 
 @InputType('CreateBoard')
+@BeforeCreateOne(CreatedByHook)
 export class BoardCreateDTO {
   @IsString()
   @Field()
--- src/domain/board/dto/update-board.dto.ts
+++ src/domain/board/dto/update-board.dto.ts
@@ -1,7 +1,10 @@
 import { Field, InputType } from '@nestjs/graphql';
+import { BeforeUpdateOne } from '@ptc-org/nestjs-query-graphql';
 import { IsBoolean } from 'class-validator';
+import { UpdatedByHook } from './nestjs-query-before-update-one.hook';
 
 @InputType('UpdateBoard')
+@BeforeUpdateOne(UpdatedByHook)
 export class BoardUpdateDTO {
   @IsBoolean()
   @Field()
