dc1c39a8567095561ad5b5fee66b596b5f67ae2a
JooYoung2274 <jooooo2274@gmail.com>
2024-01-19 13:34:05 +0900

    TypeOrmQueryService 상속 메서드 리스트

--- src/domain/post/post.service.ts
+++ src/domain/post/post.service.ts
@@ -12,3 +12,26 @@ export class PostService extends TypeOrmQueryService<PostEntity> {
     super(repo, { useSoftDelete: true });
   }
 }
+
+// TypeOrmQueryService<ENTITY> 를 상속받아 사용하기 때문에 CRUD를 따로 설정하지 않아도 사용할 수 있음
+// 세팅되어있는 옵션은 아래와 같음
+// query
+// findById
+// getById
+// createMany
+// createOne
+// updateMany
+// updateOne
+// deleteMany
+// deleteOne
+// aggregate
+// count
+// queryRelations
+// aggregateRelations
+// countRelations
+// findRelation
+// addRelations
+// setRelations
+// setRelation
+// removeRelations
+// removeRelation
